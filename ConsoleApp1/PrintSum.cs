b0ffbdf1d444e421ab1f1ec5949fda11093126be
paxkharybdis <35146073+paxkharybdis@users.noreply.github.com>
2018-11-09 23:35:47 -0600

    First solution.

---
+++ ConsoleApp1/PrintSum.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Exercises
+{
+    public class PrintSum
+    {
+        int numberToSum;
+        int sum = 0;
+        //int summedUp;
+
+        public int RequestInput()
+        {
+            Console.WriteLine("Enter a number:");
+            numberToSum = int.Parse(Console.ReadLine());
+
+            for (int n = 1; n <= numberToSum; n++)
+            {
+                sum = sum + n;
+            }
+            Console.WriteLine("The sum of 1 to " + numberToSum + " is " + sum);
+
+            return numberToSum;
+
+
+
+        }
+
+
+        public void AddSum()
+        {
+
+            //return numberToSum;  
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            NumberSum();
+        }
+
+
+
+        public static void NumberSum()
+        {
+            PrintSum newSum = new PrintSum();
+            newSum.RequestInput();
+            PrintSum newAdd = new PrintSum();
+            newAdd.AddSum();
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
